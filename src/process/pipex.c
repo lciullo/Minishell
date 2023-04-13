28edd3a4f728d6bf8566f2c6e4b0b2679af4a5b6
Clemence Llovio <cllovio@z4r6p5.42lyon.fr>
2023-04-13 16:38:34 +0200

    main.c created

--- src/process/pipex.c
+++ src/process/pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lciullo <lciullo@student.42.fr>            +#+  +:+       +#+        */
+/*   By: cllovio <cllovio@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 09:29:41 by lciullo           #+#    #+#             */
-/*   Updated: 2023/04/05 14:43:13 by lciullo          ###   ########.fr       */
+/*   Updated: 2023/04/13 15:42:36 by cllovio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,20 @@ void	call_perror(t_pipex *data)
 	exit (1);
 }
 
-int	main(int ac, char **av, char **env)
-{
-	t_pipex		data;
+// int	main(int ac, char **av, char **env)
+// {
+// 	t_pipex		data;
 
-	if (ac != 5)
-		return (ft_putstr_fd("put only 5 arguments\n", 2), 0);
-	init_struct(&data);
-	check_files(av[1], av[ac -1], &data);
-	if (get_path_env(&data, env) == -1)
-		return (0);
-	exec_cmd(&data, av, env);
-	close_files(&data);
-	waitpid(data.pid1, NULL, 0);
-	waitpid(data.pid2, NULL, 0);
-	free_env_path(&data);
-	return (0);
-}
+// 	if (ac != 5)
+// 		return (ft_putstr_fd("put only 5 arguments\n", 2), 0);
+// 	init_struct(&data);
+// 	check_files(av[1], av[ac -1], &data);
+// 	if (get_path_env(&data, env) == -1)
+// 		return (0);
+// 	exec_cmd(&data, av, env);
+// 	close_files(&data);
+// 	waitpid(data.pid1, NULL, 0);
+// 	waitpid(data.pid2, NULL, 0);
+// 	free_env_path(&data);
+// 	return (0);
+// }
