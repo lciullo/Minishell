fb8c3cbccc20f15b786b859465804e51f23af18c
Lisa Ciullo <lciullo@z4r6p1.42lyon.fr>
2023-04-14 13:09:38 +0200

    git restore ouspi

--- head/minishell.h
+++ head/minishell.h
@@ -1,28 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lciullo <lciullo@student.42.fr>            +#+  +:+       +#+        */
+/*   By: cllovio <cllovio@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/14 11:05:18 by lciullo           #+#    #+#             */
-/*   Updated: 2023/04/14 11:20:07 by lciullo          ###   ########.fr       */
+/*   Created: 2023/02/15 13:55:52 by lciullo           #+#    #+#             */
+/*   Updated: 2023/04/14 09:47:37 by cllovio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#ifndef PIPEX_H
+# define PIPEX_H
 
+# include "parsing.h"
+# include "exec.h"
 # include <sys/wait.h>
 # include <stdio.h>
+# include "../libft/headers/libft.h"
 # include <string.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-
-# include "../libft/headers/libft.h"
-# include "parsing.h"
-# include "exec.h"
 
 #endif
