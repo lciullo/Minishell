48a13e132d43d25e377f20df785f33caa6f91c42
Lisa Ciullo <lciullo@z2r3p3.42lyon.fr>
2023-04-18 16:30:00 +0200

    not point o please add new comment in execution.c

--- src/execution/pipex.c
+++ src/execution/pipex.c
@@ -6,9 +6,21 @@
 /*   By: lciullo <lciullo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 09:29:41 by lciullo           #+#    #+#             */
-/*   Updated: 2023/04/18 15:55:15 by lciullo          ###   ########.fr       */
+/*   Updated: 2023/04/18 16:27:16 by lciullo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*t_pipex		data;
+
+	init_struct(&data);
+	check_files(av[1], av[ac -1], &data);
+	if (get_path_env(&data, env) == -1)
+		return (0);
+	exec_cmd(&data, av, env);
+	close_files(&data);
+	waitpid(data.pid1, NULL, 0);
+	waitpid(data.pid2, NULL, 0);
+	free_env_path(&data);*/
 
 #include "minishell.h"
 
@@ -35,6 +47,5 @@ void	execution(char *line, char **env)
 	waitpid(data.pid2, NULL, 0);
 	free_env_path(&data);*/
 	(void)env;
-	ft_dprintf(1, "line %s\n", line);
-	ft_dprintf(1, "here\n");
+	(void)line;
 }
