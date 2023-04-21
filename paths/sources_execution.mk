533946fa6e419a56a653fafc69de3495d86146fd
Lisa Ciullo <lciullo@z2r3p7.42lyon.fr>
2023-04-21 16:26:01 +0200

    add another version of heredoc, to do : trim for expand, add infile, and done copy of begin of my list to loop this

--- paths/sources_execution.mk
+++ paths/sources_execution.mk
@@ -6,11 +6,12 @@
 #    By: lciullo <lciullo@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/14 09:55:39 by lciullo           #+#    #+#              #
-#    Updated: 2023/04/19 09:09:42 by lciullo          ###   ########.fr        #
+#    Updated: 2023/04/21 15:31:36 by lciullo          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-SRC	=																		\
-		src/execution/check_files.c											\
-		src/execution/pipex.c 												\
-		src/execution/struct.c												
+SRC	=																	\
+		src/execution/heredoc.c											\
+		src/execution/pipex.c 											\
+		src/execution/struct.c											\
+		src/execution/files.c	
