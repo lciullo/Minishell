7448313651cbe529c3ce8dd26089efd0df8e8a33
Lisa Ciullo <lciullo@z4r9p5.42lyon.fr>
2023-05-10 10:33:58 +0200

    attempt to pull origin master

--- src/execution/temporary/creat_list.c
+++ src/execution/temporary/creat_list.c
@@ -6,7 +6,7 @@
 /*   By: lciullo <lciullo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 10:54:55 by lciullo           #+#    #+#             */
-/*   Updated: 2023/05/09 18:00:15 by lciullo          ###   ########.fr       */
+/*   Updated: 2023/05/10 10:17:08 by lciullo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,26 +26,8 @@ Pipe = 3
 void	tmp_lst(t_list **lst)
 {
 	char			**infile;
-	char			**outfile;
-	char			**cmd1;
-	char			**pipe;
-	char			**cmd3;
-	char			**pipe1;
-	char			**cmd4;
 
 	infile = ft_split("< infile", ' ');
-	cmd1 = ft_split("cat Makefile", ' ');
-	pipe = ft_split("|", ' ');
-	cmd3 = ft_split("rev", ' ');
-	outfile = ft_split("> test", ' ');
-	pipe1 = ft_split("|", ' ');
-	cmd4 = ft_split("pwd", ' ');
 	ft_lstadd_back(lst, ft_lstnew(infile, 0));
-	ft_lstadd_back(lst, ft_lstnew(cmd1, 1));
-	ft_lstadd_back(lst, ft_lstnew(pipe, 3));
-	ft_lstadd_back(lst, ft_lstnew(cmd3, 1));
-	ft_lstadd_back(lst, ft_lstnew(outfile, 2));
-	ft_lstadd_back(lst, ft_lstnew(pipe1, 3));
-	ft_lstadd_back(lst, ft_lstnew(cmd4, 1));
 	return ;
 }
--- src/main.c
+++ src/main.c
@@ -48,7 +48,7 @@ static	void	loop_of_prompt(char **env)
 			add_history(line);
 		list = parsing(line);
 		//list_print(list);
-		execution(line, env, &data);
+		execution(&list, env, &data);
 		free(line);
 		ft_lstclear(&list, free);
 	}
